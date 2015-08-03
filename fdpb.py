e9d52b907a917279eaa8a9ffe9957aaf0f4622f4
András Veres-Szentkirályi <vsza@vsza.hu>
2015-08-03 09:50:27 +0200

    ignore sockets and deleted files

--- fdpb.py
+++ fdpb.py
@@ -17,7 +17,7 @@ while True:
         fds = os.listdir('/proc/{0}/fd'.format(pid))
         for fd in fds:
             link = os.readlink('/proc/{0}/fd/{1}'.format(pid, fd))
-            if link.startswith('/dev/') or link.startswith('pipe:[') or os.path.isdir(link):
+            if link.startswith('/dev/') or link.startswith('pipe:[') or link.startswith('socket:[') or os.path.isdir(link) or link.endswith(' (deleted)'):
                 continue
             print link
             fdinfo = file('/proc/{0}/fdinfo/{1}'.format(pid, fd)).read()
